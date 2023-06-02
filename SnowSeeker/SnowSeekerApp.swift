136feb6954562a9a3d3fb04a6dfa5220660b96e0
Nikolomoec <nikolomoec@gmail.com>
2023-06-02 20:09:47 +0300

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Nikita Kolomoec on 02.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
