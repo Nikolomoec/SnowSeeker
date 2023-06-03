627c879d74eb79f971cb57f082c99283b8631fd9
Nikolomoec <nikolomoec@gmail.com>
2023-06-03 18:11:18 +0300

    • Make NavigationView work in landscape

---
+++ SnowSeeker/WelcomeView.swift
@@ -0,0 +1,25 @@
+//
+//  WelcomeView.swift
+//  SnowSeeker
+//
+//  Created by Nikita Kolomoec on 03.06.2023.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        VStack {
+            Text("Welcome to SnowSeeker!")
+                .font(.largeTitle)
+            Text("Please select a resort from a left-hand menu; swipe from the left edge to show it.")
+                .foregroundColor(.secondary)
+        }
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
